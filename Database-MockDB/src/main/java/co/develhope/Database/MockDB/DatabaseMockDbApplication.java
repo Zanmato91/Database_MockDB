b1ca5d82a38e8a934b37d4b934fc926740f91a5e
Alessandro Marinelli <a.marinelli1991@gmail.com>
2022-06-13 10:08:06 +0200

    Initial commit

---
+++ Database-MockDB/src/main/java/co/develhope/Database/MockDB/DatabaseMockDbApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.Database.MockDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseMockDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseMockDbApplication.class, args);
+	}
+
+}
---
+++ Database-MockDB/src/test/java/co/develhope/Database/MockDB/DatabaseMockDbApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.Database.MockDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseMockDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
